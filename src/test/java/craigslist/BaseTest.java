23f55d6784b8c99c36e23becbb11930a57f69a62
R.O. <ramos16qa@gmail.com>
2024-02-15 13:28:04 -0700

    Commit 3_7

---
+++ src/test/java/craigslist/BaseTest.java
@@ -0,0 +1,23 @@
+package craigslist;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    WebDriver driver = new ChromeDriver();
+
+    @BeforeTest
+    public void setUp() {
+//   	WebDriverManager.chromedriver().setup();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
+}
--- src/test/java/craigslist/TestSearchPrius.java
+++ src/test/java/craigslist/TestSearchPrius.java
@@ -1,54 +1,27 @@
 package craigslist;
-
-import java.util.concurrent.TimeUnit;
-
-import org.apache.tools.ant.taskdefs.Get;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.*;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 
-public class TestSearchPrius {
 
-	public static WebDriver driver;
-
-	@BeforeTest
-	public static void setUpClass() {
-
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver() ;
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+public class TestSearchPrius extends BaseTest{
+	@Test
+	public void TestStartSearching() {
+		//		enter searching Data
 		driver.get("https://tampa.craigslist.org/search/cta?");
 		Assert.assertEquals("tampa bay cars & trucks - craigslist", driver.getTitle());
-		driver.findElement(By.id("query")).click();
-				
-	}		
-	
+		WebElement searchField = driver.findElement(By.xpath("//input[@enterkeyhint='search']"));
+		searchField.sendKeys("Prius" + "\n");
+
+	}
+
+
 	@Test
-
-	public void TestStartSearching() {
-		System.out.println("Starting Searching");
-
-		//		enter searching Data
-		WebElement searchField = driver.findElement(By.id("query"));
-		searchField.sendKeys("Prius");
-
-		//		click search button
-		WebElement searchButton = driver.findElement(By.className("searchbtn"));
-		searchButton.click();
-
-
+	public void validSearchResult(){
 
 	}
-
-	@AfterTest
-	public static void tearDown() {
-		driver.quit();
-	}
-
 }
 
 
